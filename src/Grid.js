87578b991262fc8e7de95889828b207844a3cc29
Pavel Khmelyov <pkhmelyov@gmail.com>
2020-02-28 14:22:45 +0300

    Сохраняем текущее состояние сортировки (по какому столбцу и порядок, по возрастанию или по убыванию)

--- src/Grid.js
+++ src/Grid.js
@@ -5,10 +5,29 @@ class Grid extends React.Component {
         super(props);
 
         this.columnHeader = this.columnHeader.bind(this);
+        this.onColumnHeaderClick = this.onColumnHeaderClick.bind(this);
+
+        this.state = { sorting: { column: '', asc: true } };
+    }
+
+    toggleSorting(column) {
+        const sorting = { ...this.state.sorting };
+        if(sorting.column === column) {
+            if(sorting.asc) {
+                sorting.asc = false;
+            } else {
+                sorting.column = '';
+            }
+        } else {
+            sorting.column = column;
+            sorting.asc = true;
+        }
+
+        this.setState({ sorting });
     }
 
     onColumnHeaderClick(e) {
-        alert(e.target.innerText);
+        this.toggleSorting(e.target.innerText)
     }
 
     columnHeader(caption) {
