67afa68dcd77754610b181254825872edb8c45fc
Pavel Khmelyov <pkhmelyov@gmail.com>
2020-02-28 12:41:46 +0300

    Создал новый компонент для будущей таблички

---
+++ src/Grid.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Grid extends React.Component {
+    render() {
+        return (
+            <div>Здесь будет табличка</div>
+        );
+    }
+}
+
+export default Grid;
