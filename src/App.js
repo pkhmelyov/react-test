6a131f19138da0235ac5feb180ff3d9552ad8294
Pavel Khmelyov <pkhmelyov@gmail.com>
2020-02-28 12:52:02 +0300

    Передаем компоненту на вход одну строку тестовых данных

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 import Grid from './Grid';
 
 function App() {
-  return (<Grid />);
+  return (<Grid data={ [ {
+    id: 469,
+    firstName: "Charlene",
+    lastName: "Willman",
+    email: "KJensen@magna.net",
+    phone: "(184)717-8321",
+    description: "mi nec mi lorem ipsum dolor lacus non vestibulum amet sed et at lectus tincidunt molestie ac mattis sed ipsum placerat nec massa vestibulum ac pretium tortor massa velit eros molestie pulvinar"
+} ] }/>);
 }
 
 export default App;
