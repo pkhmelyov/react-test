a975e262647d2e51bc7958c03af20bf5b66ea71a
Pavel Khmelyov <pkhmelyov@gmail.com>
2020-02-28 12:49:34 +0300

    Учим компонент принимать данные для отображения в табличке

--- src/Grid.js
+++ src/Grid.js
@@ -2,6 +2,14 @@ import React from 'react';
 
 class Grid extends React.Component {
     render() {
+        const { data } = this.props;
+
+        if(!(data && data.length)) {
+            return (
+                <div>Нет данных...</div>
+            );
+        }
+
         return (
             <div>Здесь будет табличка</div>
         );
