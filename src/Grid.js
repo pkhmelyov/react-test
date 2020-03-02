7e5c5e94f7d04fc8e6270b7a675a0391429e9043
Pavel Khmelyov <pkhmelyov@gmail.com>
2020-03-02 12:39:52 +0300

    Пофиксил баг в отключении сортировки

--- src/Grid.js
+++ src/Grid.js
@@ -53,7 +53,7 @@ class Grid extends React.Component {
             filteredRows = [ ...this.props.data ];
             this.setState({ filteredRows });
         }
-        return filteredRows;
+        return [ ...filteredRows ];
     }
 
     getSortedRows() {
