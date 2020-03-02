66e3ddc4bf6d243fec71f7d6df4278dfd556dd80
Pavel Khmelyov <pkhmelyov@gmail.com>
2020-03-02 16:28:21 +0300

    Чтобы всё было по красоте :)

--- src/components/columnHeader.js
+++ src/components/columnHeader.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import SortingIndicator from './sortingIndicator';
 
+import './columnHeader.css';
+
 function ColumnHeader({ definition, sortingState, onClick }) {
     const { key, text } = definition;
     return (
-        <th onClick={onClick(key)}>
+        <th className='column-header' onClick={onClick(key)}>
             {text}
             <SortingIndicator columnKey={key} sortingState={sortingState} />
         </th>
